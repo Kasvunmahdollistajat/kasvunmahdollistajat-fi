6e24f23c5dad605a098230ee70a28029b15b9325
Markus <markus.tyrkko@gmail.com>
2022-03-28 21:54:07 +0300

    Improve jumbo styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,14 @@ const Home = () => {
   return (
     <>
       <div className={styles.jumbo}>
-        <img src="/Kivet.jpg" alt="" />
         <div className={styles.jumboWrapper}>
-          <h1>KASVUN MAHDOLLISTAJAT</h1>
-          <h2 style={{ fontSize: "2rem" }}>
-            Autamme organisaatioita onnistumaan ja ihmisiä kasvamaan
-            johtajuudessa
-          </h2>
+          <div>
+            <h1>KASVUN MAHDOLLISTAJAT</h1>
+            <h2>
+              Autamme organisaatioita onnistumaan ja ihmisiä kasvamaan
+              johtajuudessa
+            </h2>
+          </div>
         </div>
       </div>
       <div className={styles.content}>
