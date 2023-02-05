3600a1e427afbb41940716c9a6d301f4a8f47a0c
Markus <markus.tyrkko@gmail.com>
2023-02-05 17:45:10 +0200

    New content

--- pages/components/NavBar.tsx
+++ pages/components/NavBar.tsx
@@ -1,53 +1,9 @@
-import { useState } from "react";
-import { Link } from "react-scroll";
 import styles from "./NavBar.module.scss";
 
-const ScrollLink = ({
-  target,
-  children,
-}: {
-  target: string;
-  children: string | JSX.Element;
-}) => (
-  <Link to={target} smooth={true} offset={0} duration={500}>
-    {children}
-  </Link>
-);
-
-const classnames = (baseClass: string, menuOpen: boolean): string =>
-  `${baseClass ? `${baseClass} ` : ""}${
-    menuOpen ? styles.visible : styles.hidden
-  }`;
-
 const NavBar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
   return (
     <div className={styles.navBar}>
       <img src="Logo.png" alt="logo" />
-      <div className={classnames(styles.linkWrapper, menuOpen)}>
-        <ScrollLink target={"palvelut"}>Palvelut</ScrollLink>
-        <ScrollLink target={"tarina"}>Tarina</ScrollLink>
-        <ScrollLink target={"asiantuntijat"}>Asiantuntijat</ScrollLink>
-      </div>
-      <div className={styles.mobileMenu}>
-        <img
-          src="menu.webp"
-          alt={"Valikko"}
-          className={classnames("", !menuOpen)}
-          onClick={() => {
-            setMenuOpen(true);
-          }}
-        />
-        <img
-          src="close.webp"
-          alt={"X"}
-          className={classnames("", menuOpen)}
-          onClick={() => {
-            setMenuOpen(false);
-          }}
-        />
-      </div>
     </div>
   );
 };
--- pages/home/intro.tsx
+++ pages/home/intro.tsx
@@ -1,40 +1,21 @@
+import styles from "./intro.module.scss";
+
 const Intro = () => (
-  <div>
-    <p>
-      Kasvun mahdollistajat Oy on palvelubisneksen ja johtamisen kehittäjä. Saat
-      käyttöösi laajan osaamisemme, joka pohjautuu monipuoliseen käytännön
-      kokemukseen mm. johtajan työstä eri organisaatioissa. Emme tee mitään
-      vasemmalla kädellä. Paneudumme sinun ja organisaatiosi tilanteeseen
-      aidosti ja syvästi. Tuomme uutta ajattelua, avaamme solmuja ja kuljemme
-      rinnalla inhimillisen empaattisesti. Haastamme teitä kehittymään niin
-      työroolissa kuin ihmisinä.
-    </p>
-    <p>
-      Palvelukokonaisuuden rakennamme sinulle ja yrityksellesi aina
-      räätälöidysti. Käytämme innostavia ja oivalluttavia työmenetelmiä ja
-      työkaluja. Teemme työtämme tavoitteellisesti ja järjestelmällisesti.
-      Takaamme, että yhteistyömme on vaikuttavaa: toteutatte strategiaa ja
-      etenette tavoitteisanne nopeammin, tehokkaammin ja tuloksellisemmin.
-      Yhteistyö kanssamme on energisoivaa ja sujuu mutkattomasti, sanovat
-      asiakkaamme. He arvostavat kirkasta ajatteluamme, sitoutumistamme ja
-      inhimillistä otettamme.
-    </p>
-    <p>
-      Kasvun mahdollistajat Oy on kuin luottoystäväsi: luottamuksesi arvoinen
-      briljantti ammattilainen.
-    </p>
-    <p>
-      Hyödynnä Business Finlandin innovaatioseteli: Kasvun Mahdollistajat Oy on
-      hyväksytty innovaatiosetelin palveluntuottaja. Hae innovaatioseteliä{" "}
-      <a
-        href="https://www.businessfinland.fi/suomalaisille-asiakkaille/palvelut/rahoitus/tutkimus-ja-kehitysrahoitus/innovaatioseteli/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        täältä
-      </a>
-      .
-    </p>
+  <div className={styles.intro}>
+    <div className={styles.column}>
+      <span>
+        <b>Kasvun mahdollistajat Oy</b>
+      </span>
+      <span>Mikko Tyrkkö</span>
+      <span>B-to-B myynti ja myynnin konsultointi</span>
+      <span>
+        Ota yhteyttä:{" "}
+        <a href="mailto:miktyrkko@gmail.com">miktyrkko@gmail.com</a>
+      </span>
+    </div>
+    <div className={styles.column}>
+      <img src="mikko.jpg" alt="Mikko" />
+    </div>
   </div>
 );
 
